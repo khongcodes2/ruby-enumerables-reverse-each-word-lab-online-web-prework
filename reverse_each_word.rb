ea203dc7c1fe3a1829ff16261388570afe4a74de
K Hong <albtrossfacefall@gmail.com>
2019-08-13 13:19:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,10 @@
 def reverse_each_word(input_str)
   #turn string into Array
   word_str=input_str.split
+  holder=[]
   #for each element in array, create new array
   word_str.collect do |word|
-    word.split(//).reverse.join.join
+    holder.push(word.split(//).reverse.join)
   end
+  holder.join
 end
